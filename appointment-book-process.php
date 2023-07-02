8929359b697b7c0de65048d2edc3c123479c1e69
Knize-Kazzele <102401298+joshua091921@users.noreply.github.com>
2023-07-03 00:37:56 +0800

    add mailer response

--- appointment-book-process.php
+++ appointment-book-process.php
@@ -2,6 +2,10 @@
 session_start();
 include 'config.php';
 
+require("PHPMailer/src/PHPMailer.php");
+require("PHPMailer/src/Exception.php");
+require("PHPMailer/src/SMTP.php");
+
 // Check if the user is logged in, if not then redirect him to the login page
 if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
     header("location: index.php");
@@ -81,9 +85,119 @@ if (isset($_POST['submit'])) {
             'end_time' => $appointment_end_time
         ];
 
-        // Redirect to appointment-book.php with the response as a query parameter
-        header("location: appointment-book.php?response=" . urlencode(json_encode($response)));
-        exit();
+        // Prepare and send the email
+        $mail = new PHPMailer\PHPMailer\PHPMailer();
+        
+        // Set up SMTP configuration
+        $mail->isSMTP();
+        $mail->Host = 'smtp.gmail.com';
+        $mail->SMTPDebug = 0;
+        $mail->Port = 587;
+        $mail->SMTPSecure = 'tls';
+        $mail->SMTPAuth = true;
+        $mail->Username = 'healteethph@gmail.com';
+        $mail->Password = 'lektzbyfwtljbqpd';
+
+        // Set up sender and recipient
+        $mail->setFrom('healteethph@gmail.com', 'Official Healteeth');
+        $mail->addAddress($email, $patient_name);
+
+        // Attach the logo image as CID
+        $logoPath = 'assets/image/Healteeth Logo.png';
+        $cid = $mail->addEmbeddedImage($logoPath, 'logo', 'logo.png');
+
+        // Set email content
+        $mail->Subject = 'Appointment';
+        $mail->isHTML(true); // Set email content type
+        
+        // Email body with CSS design
+        $mail->Body = '
+            <html>
+            <head>
+            <style>
+            /* Reset CSS */
+            body, h1, h2, h3, h4, h5, h6, p, ul, ol, li {
+               margin: 0;
+               padding: 0;
+            }
+            body {
+               font-family: Arial, sans-serif;
+               background-color: #f1f1f1;
+            }
+            /* Container */
+            .container {
+               max-width: 600px;
+               margin: 0 auto;
+               border: 1px solid #dddddd;
+               border-radius: 6px;
+            }
+            /* Header */
+            .header {
+              border-bottom: thin solid #dadce0;
+               padding: 30px;
+               text-align: center;
+               margin-left: 10%;
+               margin-right: 10%;
+            }
+            .header img {
+               max-width: 150px;
+               margin-bottom: 20px;
+            }
+            /* Content */
+            .content {
+               padding: 30px;
+               color: #333333;
+               line-height: 1.5;
+            }
+            .content h2 {
+               font-size: 24px;
+               margin-bottom: 20px;
+            }
+            .content p {
+               font-size: 16px;
+               margin-bottom: 10px;
+            }
+            /* Footer */
+            .footer {
+               background-color: #f1f1f1;
+               padding: 30px;
+               text-align: center;
+            }
+            .footer p {
+               font-size: 14px;
+               margin-bottom: 10px;
+            }
+         </style>
+            </head>
+            <body>
+            <div class="container">
+            <div class="header">
+               <img src="cid:logo" alt="Healteeth PH Logo">
+               <br>
+               <h3>Scheduled Appointment</h3>
+            </div>
+            <div class="content">
+               <h2>Dear '.$patient_name.',</h2>
+               <p>Thank you for booking an appointment with us. We would like to confirm that your appointment has been successfully scheduled.
+                <br>
+                If you have any questions or need to make any changes, please feel free to contact us.
+                <br>We look forward to meeting you on '.$date_sched.'.</p>
+               <br>
+               <p>Thank you,</p>
+               <p>'.$patient_name.'</p>
+            </div>
+                <p style="text-align: center;">&copy; '.date("Y").' Healteeth PH. All rights reserved.</p>
+            </body>
+            </html>';
+
+        // Check if the email was sent successfully
+        if ($mail->send()) {
+            // Redirect to appointment-book.php with the response as a query parameter
+            header("location: appointment-book.php?response=" . urlencode(json_encode($response)));
+            exit();
+        } else { 
+            echo 'Email could not be sent. Error: ' . $mail->ErrorInfo;
+        }
     } else if ($schedule == "Schedule 2") {
         $start_time = max($last_appointment_time, $breaktime_end);
         $end_time = $start_time + (strtotime($service_duration) - strtotime('00:00'));
@@ -105,9 +219,119 @@ if (isset($_POST['submit'])) {
             'end_time' => $appointment_end_time
         ];
 
-        // Redirect to appointment-book.php with the response as a query parameter
-        header("location: appointment-book.php?response=" . urlencode(json_encode($response)));
-        exit();
+        // Prepare and send the email
+        $mail = new PHPMailer\PHPMailer\PHPMailer();
+        
+        // Set up SMTP configuration
+        $mail->isSMTP();
+        $mail->Host = 'smtp.gmail.com';
+        $mail->SMTPDebug = 0;
+        $mail->Port = 587;
+        $mail->SMTPSecure = 'tls';
+        $mail->SMTPAuth = true;
+        $mail->Username = 'healteethph@gmail.com';
+        $mail->Password = 'lektzbyfwtljbqpd';
+
+        // Set up sender and recipient
+        $mail->setFrom('healteethph@gmail.com', 'Official Healteeth');
+        $mail->addAddress($email, $patient_name);
+
+        // Attach the logo image as CID
+        $logoPath = 'assets/image/Healteeth Logo.png';
+        $cid = $mail->addEmbeddedImage($logoPath, 'logo', 'logo.png');
+
+        // Set email content
+        $mail->Subject = 'Appointment';
+        $mail->isHTML(true); // Set email content type
+        
+        // Email body with CSS design
+        $mail->Body = '
+            <html>
+            <head>
+            <style>
+            /* Reset CSS */
+            body, h1, h2, h3, h4, h5, h6, p, ul, ol, li {
+               margin: 0;
+               padding: 0;
+            }
+            body {
+               font-family: Arial, sans-serif;
+               background-color: #f1f1f1;
+            }
+            /* Container */
+            .container {
+               max-width: 600px;
+               margin: 0 auto;
+               border: 1px solid #dddddd;
+               border-radius: 6px;
+            }
+            /* Header */
+            .header {
+              border-bottom: thin solid #dadce0;
+               padding: 30px;
+               text-align: center;
+               margin-left: 10%;
+               margin-right: 10%;
+            }
+            .header img {
+               max-width: 150px;
+               margin-bottom: 20px;
+            }
+            /* Content */
+            .content {
+               padding: 30px;
+               color: #333333;
+               line-height: 1.5;
+            }
+            .content h2 {
+               font-size: 24px;
+               margin-bottom: 20px;
+            }
+            .content p {
+               font-size: 16px;
+               margin-bottom: 10px;
+            }
+            /* Footer */
+            .footer {
+               background-color: #f1f1f1;
+               padding: 30px;
+               text-align: center;
+            }
+            .footer p {
+               font-size: 14px;
+               margin-bottom: 10px;
+            }
+         </style>
+            </head>
+            <body>
+            <div class="container">
+            <div class="header">
+               <img src="cid:logo" alt="Healteeth PH Logo">
+               <br>
+               <h3>Scheduled Appointment</h3>
+            </div>
+            <div class="content">
+               <h2>Dear '.$patient_name.',</h2>
+               <p>Thank you for booking an appointment with us. We would like to confirm that your appointment has been successfully scheduled.
+                <br>
+                If you have any questions or need to make any changes, please feel free to contact us.
+                <br>We look forward to meeting you on '.$date_sched.'.</p>
+               <br>
+               <p>Thank you,</p>
+               <p>'.$patient_name.'</p>
+            </div>
+                <p style="text-align: center;">&copy; '.date("Y").' Healteeth PH. All rights reserved.</p>
+            </body>
+            </html>';
+
+        // Check if the email was sent successfully
+        if ($mail->send()) {
+            // Redirect to appointment-book.php with the response as a query parameter
+            header("location: appointment-book.php?response=" . urlencode(json_encode($response)));
+            exit();
+        } else {
+            echo 'Email could not be sent. Error: ' . $mail->ErrorInfo;
+        }
     }
 }
 ?>
