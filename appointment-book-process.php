88b0df61eabd9c5a68a2adb595a827e4736a8560
xiphoidProcess1457 <74455841+xiphoidProcess1457@users.noreply.github.com>
2023-07-10 17:33:47 +0800

    added mailer to appoitment book

--- appointment-book-process.php
+++ appointment-book-process.php
@@ -2,6 +2,14 @@
 session_start();
 include 'config.php';
 
+require("PHPMailer/src/PHPMailer.php");
+require("PHPMailer/src/Exception.php");
+require("PHPMailer/src/SMTP.php");
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\SMTP;
+
 // Check if the user is logged in, if not then redirect him to the login page
 if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
     header("location: index.php");
@@ -102,13 +110,154 @@ if (isset($_POST['submit'])) {
             $appointment_start_time = date('h:i A', $start_time);
             $appointment_end_time = date('h:i A', $end_time);
 
-            $response = [
-                'success' => true,
-                'message' => 'Appointment booked successfully!',
-                'appointment_date' => $appointment_date,
-                'start_time' => $appointment_start_time,
-                'end_time' => $appointment_end_time
-            ];
+            // Send email notification
+            $subject = 'Appointment Confirmation';
+//             $message = <<<HTML
+// <!DOCTYPE html>
+// <html>
+//     <head>
+//         <style>
+//             /* CSS styles for the email */
+//         </style>
+//     </head>
+//     <body>
+//         <h2>Appointment Confirmation</h2>
+//         <p>Dear $full_name,</p>
+//         <p>Your appointment has been booked successfully!</p>
+//         <p>Appointment Details:</p>
+//         <ul>
+//             <li>Patient Name: $patient_name</li>
+//             <li>Email: $email</li>
+//             <li>Phone: $phone</li>
+//             <li>Address: $address</li>
+//             <li>Category: $categorypick</li>
+//             <li>Service: $servicepick</li>
+//             <li>Date: $appointment_date</li>
+//             <li>Start Time: $appointment_start_time</li>
+//             <li>End Time: $appointment_end_time</li>
+//         </ul>
+//         <p>Thank you for choosing our services.</p>
+//         <p>Best regards,<br>Healteeth PH</p>
+//     </body>
+// </html>
+// HTML;
+
+
+
+            $mail = new PHPMailer();
+            $mail->isSMTP();
+            $mail->Host = 'smtp.gmail.com';
+            $mail->Port = 587;
+            $mail->SMTPSecure = 'tls';
+            $mail->SMTPAuth = true;
+            $mail->Username = 'healteethph@gmail.com';
+            $mail->Password = 'lektzbyfwtljbqpd';
+
+            $mail->setFrom('your_email@example.com', 'Healteeth PH'); // Replace with your email address and name
+            $mail->addAddress($email, $patient_name); // Add recipient email and name
+
+
+             // Attach the logo image as CID
+      $logoPath = 'assets/image/Healteeth Logo.png';
+      $cid = $mail->addEmbeddedImage($logoPath, 'logo', 'logo.png');
+
+
+            $mail->Subject = $subject;
+             $mail->Body = '
+         <html>
+         <head>
+            <style>
+               /* Reset CSS */
+               body, h1, h2, h3, h4, h5, h6, p, ul, ol, li {
+                  margin: 0;
+                  padding: 0;
+               }
+               body {
+                  font-family: Arial, sans-serif;
+                  background-color: #f1f1f1;
+               }
+               /* Container */
+               .container {
+                  max-width: 600px;
+                  margin: 0 auto;
+                  border: 1px solid #dddddd;
+                  border-radius: 6px;
+               }
+               /* Header */
+               .header {
+                 border-bottom: thin solid #dadce0;
+                  padding: 30px;
+                  text-align: center;
+                  margin-left: 10%;
+                  margin-right: 10%;
+               }
+               .header img {
+                  max-width: 150px;
+                  margin-bottom: 20px;
+               }
+               /* Content */
+               .content {
+                  padding: 30px;
+                  color: #333333;
+                  line-height: 1.5;
+               }
+               .content h2 {
+                  font-size: 24px;
+                  margin-bottom: 20px;
+               }
+               .content p {
+                  font-size: 16px;
+                  margin-bottom: 10px;
+               }
+               /* Footer */
+               .footer {
+                  background-color: #f1f1f1;
+                  padding: 30px;
+                  text-align: center;
+               }
+               .footer p {
+                  font-size: 14px;
+                  margin-bottom: 10px;
+               }
+            </style>
+         </head>
+         <body>
+            <div class="container">
+               <div class="header">
+                  <img src="cid:logo" alt="Healteeth PH Logo">
+                  <br>
+                  <h3>We have received your feedback!</h3>
+               </div>
+               <div class="content">
+                  <p>Appointment Reminder for <b>'.$patient_name.'</b>
+                   <br>
+                   This is a reminder your appointment for Dental Consultation<br>
+                    is on <b>'.$appointment_date.'</b> at <b>'.$appointment_start_time.'</b></p>
+                  <br>
+                  <p>Thank you,</p>
+                  <p>Healteeth Philippines</p>
+               </div>
+               
+            </div>
+             <p style="text-align: center;">&copy; '.date("Y").' Healteeth PH. All rights reserved.</p>
+         </body>
+         </html>';
+            $mail->isHTML(true);
+
+            if ($mail->send()) {
+                $response = [
+                    'success' => true,
+                    'message' => 'Appointment booked successfully!',
+                    'appointment_date' => $appointment_date,
+                    'start_time' => $appointment_start_time,
+                    'end_time' => $appointment_end_time
+                ];
+            } else {
+                $response = [
+                    'success' => false,
+                    'message' => 'Appointment booked successfully, but failed to send email notification.'
+                ];
+            }
 
             // Redirect to appointment-book.php with the response as a query parameter
             header("location: appointment-book.php?response=" . urlencode(json_encode($response)));
--- appointment-book.php
+++ appointment-book.php
@@ -47,7 +47,8 @@
    <head>
       <meta charset="utf-8">
       <meta name="viewport" content="width=device-width, initial-scale=1">
-      <title>Book an Appointment</title>
+      <title>Book an Appointment| HealTeeth</title>
+<link rel="icon" type="image/x-icon" href="assets/healteeth.ico">
       <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
       <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.bundle.min.js"></script>
       <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
